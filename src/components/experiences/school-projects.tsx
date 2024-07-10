a89612c389c1ea60afc60f59a30094477cb486f5
Ugolin Olle <hello@ugolin-olle.com>
2024-07-10 21:04:11 +0700

    fix(ui): update grid responsive

--- src/components/experiences/school-projects.tsx
+++ src/components/experiences/school-projects.tsx
@@ -5,7 +5,7 @@ import { Picture } from '../mdx/ui';
 
 export const SchoolProjects: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-5">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-5">
       {SCHOOL_PICTURES.map((item, index) => (
         <div key={index} className="flex flex-col items-center">
           <Picture src={item.src} alt={item.alt} />
