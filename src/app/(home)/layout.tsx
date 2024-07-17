8ae5f8d219d81da07fbef194d25e4e143e193cd6
Ugolin Olle <hello@ugolin-olle.com>
2024-07-17 15:35:07 +0700

    feat(ui): update shadow + padding space

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -12,10 +12,10 @@ export default function HomeLayout({
   return (
     <MainContainer>
       <div className="w-full lg:w-[70vw] h-full flex flex-col justify-center gap-3 lg:gap-5 lg:justify-between">
-        <div className="h-full lg:h-2/5 p-2 my-5 md:p-8">
+        <div className="h-full lg:h-2/5 p-2 mt-5 mb-2 md:px-8 md:pt-8">
           <Hero />
         </div>
-        <div className="h-full lg:h-3/5 p-2 md:pb-8 md:px-8">
+        <div className="h-full lg:h-auto p-2 md:pb-8 md:px-8">
           <Navbar />
           {children}
         </div>
