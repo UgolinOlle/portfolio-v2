6cc5b52f7eaf404de06cef231304e373838c9e50
Ugolin Olle <hello@ugolin-olle.com>
2024-07-20 17:48:13 +0700

    feat(core): adding uses & lab pages

---
+++ src/app/(home)/lab/page.tsx
@@ -0,0 +1,11 @@
+import { AnimatedContainer } from '@/components/commons/animation';
+import { Heading } from '@/components/ui/headers';
+
+export default function LabPage() {
+  // -- Render
+  return (
+    <AnimatedContainer custom={0}>
+      <Heading variant="h2">Lab</Heading>
+    </AnimatedContainer>
+  );
+}
---
+++ src/app/(home)/uses/page.tsx
@@ -0,0 +1,14 @@
+import {
+  AnimatedContainer,
+  AnimatedSection,
+} from '@/components/commons/animation';
+import { Heading } from '@/components/ui/headers';
+
+export default function UsesPage() {
+  // -- Render
+  return (
+    <AnimatedContainer custom={0}>
+      <Heading variant="h2">Mes outils</Heading>
+    </AnimatedContainer>
+  );
+}
