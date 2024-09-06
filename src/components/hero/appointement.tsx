fa39e7e9a3471ea46d8b592c0c883bbe049f3971
Ugolin Ollé <hello@ugolin-olle.com>
2024-09-06 21:55:07 +0700

    style(ui): add opacity & reduce translate

--- src/components/hero/appointement.tsx
+++ src/components/hero/appointement.tsx
@@ -11,10 +11,10 @@ export const Appointement: React.FC = () => {
   return (
     <Link href="https://cal.com/ugolin-olle" passHref target="_blank">
       <Button className="relative bg-primary text-white dark:bg-white dark:text-black flex items-center justify-center group/modal-btn shadow-md hover:shadow-lg border border-black/5 dark:border-white/5 transition duration-300 ease-in-out">
-        <div className="gap-2 group-hover/modal-btn:translate-x-40 text-center transition duration-500 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium">
+        <div className="gap-2 group-hover/modal-btn:translate-x-40 group-hover/modal-btn:opacity-0 text-center transition duration-500 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium">
           <Phone size={16} /> Prendre RDV
         </div>
-        <div className="text-white dark:text-black -translate-x-32 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute transition duration-500 animate-phone">
+        <div className="text-white dark:text-black -translate-x-14 opacity-0 group-hover/modal-btn:opacity-100 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute transition duration-500 group-hover/modal-btn:animate-phone">
           <Phone size={16} />
         </div>
       </Button>
