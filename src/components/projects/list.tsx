b9c6e0af87f2c79ca00577b1bdb29c516d1d4e22
Ugolin Ollé <hello@ugolin-olle.com>
2024-09-11 11:04:24 +0700

    fix(ui): height & width for card + lazy loading for image

--- src/components/projects/list.tsx
+++ src/components/projects/list.tsx
@@ -8,7 +8,7 @@ import { MoveUpRight } from 'lucide-react';
 import { IProjectItem } from '@/lib/interfaces/project';
 import { SOCIALS_ICONS } from '@/components/commons/icons/socials';
 import { Heading } from '@/components/ui/headers';
-import { AnimatedGrid } from '../commons/animation';
+import { AnimatedGrid } from '@/components/commons/animation';
 
 /**
  * @name ProjectItem
@@ -32,12 +32,12 @@ const ProjectItem: React.FC<IProjectItem> = (props): JSX.Element => {
   // --- Render
   return (
     <li
-      className={`bg-neutral-100 dark:bg-neutral-900 shadow-md rounded-lg p-4 w-full flex flex-col items-start gap-10 min-h-[130px] max-h-[650px] border`}
+      className={`bg-neutral-100 dark:bg-neutral-900 shadow-md rounded-lg p-4 w-full flex flex-col items-start justify-between min-h-[130px] max-h-[650px] border`}
       style={{ rotate: `${hoverRotate}deg`, transition: 'rotate 0.3s' }}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className="flex flex-col items-start gap-6">
+      <div className="flex flex-col items-start gap-6 w-full">
         <div className="w-full relative" style={{ paddingBottom: '50%' }}>
           {props.imageBg && (
             <Image
@@ -46,6 +46,8 @@ const ProjectItem: React.FC<IProjectItem> = (props): JSX.Element => {
               layout="fill"
               objectFit="cover"
               className="absolute top-0 left-0 w-full h-full rounded-lg opacity-25"
+              placeholder="blur"
+              blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJncmF5Ii8+PC9zdmc+"
             />
           )}
           <Image
@@ -54,6 +56,8 @@ const ProjectItem: React.FC<IProjectItem> = (props): JSX.Element => {
             layout="fill"
             objectFit={`${props.imageSize || 'cover'}`}
             className="rounded-lg"
+            placeholder="blur"
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJncmF5Ii8+PC9zdmc+"
           />
         </div>
         <div className="flex flex-col items-start gap-2">
--- src/components/uses/card.tsx
+++ src/components/uses/card.tsx
@@ -1,6 +1,8 @@
 import Image, { StaticImageData } from 'next/image';
-import { Heading } from '../ui/headers';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
+
+import { Heading } from '@/components/ui/headers';
 
 export interface CardProps {
   title: string;
@@ -9,15 +11,10 @@ export interface CardProps {
   link: string;
 }
 
-export const UseCard: React.FC<CardProps> = ({
-  title,
-  description,
-  image,
-  link,
-}) => {
+const UseCard: React.FC<CardProps> = ({ title, description, image, link }) => {
   // --- Render
   return (
-    <div className="flex items-center justify-between rounded-lg dark:hover:bg-muted py-4 transition duration-300 ease-in-out group/uses-card">
+    <div className="flex items-center justify-between rounded-lg dark:hover:bg-muted py-4 px-2 transition duration-300 ease-in-out group/uses-card">
       <Image
         src={image}
         alt={title}
@@ -25,9 +22,13 @@ export const UseCard: React.FC<CardProps> = ({
         height="70"
         className="rounded-lg m-auto transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out"
       />
-      <div className="flex flex-col gap-2 items-start w-2/3 transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out">
+      <div className="flex flex-col gap-2 items-start w-2/3">
         <div className="flex justify-between items-center w-full">
-          <Heading variant="h3" border={false}>
+          <Heading
+            variant="h3"
+            border={false}
+            className="transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out"
+          >
             {title}
           </Heading>
           <Link
@@ -39,10 +40,69 @@ export const UseCard: React.FC<CardProps> = ({
             Obtenir
           </Link>
         </div>
-        <p className="text-neutral-600/70 dark:text-neutral-400/70">
+        <p className="text-neutral-600/70 dark:text-neutral-400/70 transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out">
           {description}
         </p>
       </div>
     </div>
   );
 };
+
+const UseCardImage: React.FC<CardProps> = ({
+  title,
+  description,
+  image,
+  link,
+}) => {
+  // --- Variantes pour l'animation du texte et du titre
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.4, ease: 'easeInOut' },
+    },
+  };
+
+  // --- Render
+  return (
+    <div className="relative flex items-center justify-center rounded-lg dark:hover:bg-muted py-4 px-2 transition duration-300 ease-in-out group/uses-card overflow-hidden">
+      {/* Image pleine page */}
+      <Image
+        src={image}
+        alt={title}
+        width="300"
+        height="300"
+        className="rounded-lg m-auto transform group-hover/uses-card:scale-105 transition duration-300 ease-in-out w-full h-full object-cover"
+      />
+
+      {/* Contenu textuel (apparition au hover) */}
+      <motion.div
+        initial="hidden"
+        whileHover="visible"
+        className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-start p-4 bg-gradient-to-t from-black/60 to-transparent"
+      >
+        <motion.div variants={textVariants}>
+          <h3 className="text-lg text-white font-semibold">{title}</h3>
+        </motion.div>
+        <motion.p variants={textVariants} className="text-neutral-200 mt-2">
+          {description}
+        </motion.p>
+
+        {/* Lien pour obtenir */}
+        <Link
+          target="_blank"
+          href={link}
+          passHref
+          className="mt-4 text-sm px-2 py-1 rounded-full text-white bg-neutral-500/70 dark:hover:text-neutral-200/70 hover:text-neutral-800/70 transition duration-300 ease-in-out"
+        >
+          Obtenir
+        </Link>
+      </motion.div>
+    </div>
+  );
+};
+
+export default UseCardImage;
+
+export { UseCard };
