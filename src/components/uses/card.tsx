67bb8662003bf8f4fbab3a205ddef57a3eac4e7c
Ugolin Ollé <hello@ugolin-olle.com>
2024-09-16 12:12:14 +0700

    fix(ui): remove hover on responsive

--- src/components/uses/card.tsx
+++ src/components/uses/card.tsx
@@ -23,14 +23,14 @@ const UseCard: React.FC<CardProps> = ({ title, description, image, link }) => {
         alt={title}
         width="70"
         height="70"
-        className="rounded-lg m-auto transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out"
+        className="rounded-lg m-auto transform lg:group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out"
       />
       <div className="flex flex-col gap-2 items-start w-2/3">
         <div className="flex justify-between items-center w-full">
           <Heading
             variant="h3"
             border={false}
-            className="transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out"
+            className="transform lg:group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out"
           >
             {title}
           </Heading>
@@ -39,13 +39,13 @@ const UseCard: React.FC<CardProps> = ({ title, description, image, link }) => {
               target="_blank"
               href={link}
               passHref
-              className="cursor-none text-sm px-2 py-1 rounded-full text-neutral-600/70 dark:text-neutral-400/70 bg-neutral-300/70 dark:bg-neutral-700/70 dark:hover:text-neutral-200/70 hover:text-neutral-800/70 transition duration-300 ease-in-out"
+              className="cursor-none text-sm px-2 py-1 rounded-full text-neutral-600/70 dark:text-neutral-400/70 bg-neutral-300/70 dark:bg-neutral-700/70 dark:hover:text-neutral-200/70 lg:hover:text-neutral-800/70 transition duration-300 ease-in-out"
             >
               Obtenir
             </Link>
           )}
         </div>
-        <p className="text-neutral-600/70 dark:text-neutral-400/70 transform group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out">
+        <p className="text-neutral-600/70 dark:text-neutral-400/70 transform lg:group-hover/uses-card:translate-x-1 transition duration-300 ease-in-out">
           {description}
         </p>
       </div>
@@ -78,7 +78,7 @@ const UseCardImage: React.FC<CardProps> = ({ title, description, image }) => {
         initial="hidden"
         whileHover="visible"
         className={cn(
-          'absolute bottom-0 left-0 w-full h-full flex flex-col justify-end items-start p-4 pb-8 opacity-0 group-hover/uses-card:opacity-100 transition duration-300 ease-in-out',
+          'absolute bottom-0 left-0 w-full h-full flex flex-col justify-end items-start p-4 pb-8 opacity-0 lg:group-hover/uses-card:opacity-100 transition duration-300 ease-in-out',
           'bg-gradient-to-t from-white/60 to-transparent'
         )}
       >
