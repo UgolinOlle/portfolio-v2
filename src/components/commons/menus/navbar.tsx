cda02a4ed9716a9a2ff64e62e337c4c3c7d9bed9
Ugolin Ollé <hello@ugolin-olle.com>
2024-10-14 01:17:01 +0700

    fix(core): rollback to default navbar

--- src/components/commons/menus/navbar.tsx
+++ src/components/commons/menus/navbar.tsx
@@ -11,7 +11,6 @@ export const Navbar: React.FC = () => {
   const pathname = usePathname();
   const [activeTab, setActiveTab] = useState('about');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleTabChange = (value: SetStateAction<string>) => {
     setActiveTab(value);
@@ -22,7 +21,6 @@ export const Navbar: React.FC = () => {
     if (path.startsWith('http')) window.open(path, '_blank');
     else router.push(path);
     setIsMenuOpen(false);
-    setIsDropdownOpen(false);
   };
 
   useEffect(() => {
@@ -51,86 +49,11 @@ export const Navbar: React.FC = () => {
       </div>
 
       {/* --- blur effect ----------------------------------------------------------------------------------------- */}
-      <div
-        className={`${
-          isDropdownOpen ? 'backdrop-blur-lg' : ''
-        } w-full flex-col items-start justify-between lg:flex lg:flex-row lg:items-center`}
-      >
+      <div className={`w-full flex-col items-start justify-between lg:flex lg:flex-row lg:items-center`}>
         <Tabs value={activeTab} onValueChange={handleTabChange} className="w-full lg:w-auto">
           <TabsList className="flex h-auto flex-col lg:flex-grow lg:flex-row lg:justify-start">
-            {/* --- About me -------------------------------------------------------------------------------------- */}
-            <div
-              onMouseEnter={() => setIsDropdownOpen(true)}
-              onMouseLeave={() => setIsDropdownOpen(false)}
-              className="relative w-full lg:w-auto"
-            >
-              <TabsTrigger value="À propos" className="w-full cursor-none lg:w-auto">
-                À propos
-              </TabsTrigger>
-
-              <AnimatePresence>
-                {isDropdownOpen && (
-                  <motion.div
-                    initial={{ opacity: 0, y: -10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -10 }}
-                    className="absolute left-0 mt-2 w-[450px] rounded-2xl bg-white shadow-lg lg:mt-4"
-                  >
-                    <ul className="flex flex-col items-start gap-3 p-2">
-                      <li
-                        className="flex w-full cursor-pointer items-center rounded-xl bg-neutral-100 p-2"
-                        onClick={() => handleNavigation('/#me', 'Moi')}
-                      >
-                        <div className="relative mr-5 h-full">
-                          <motion.div
-                            initial={{ rotate: 0 }}
-                            whileHover={{ rotate: 14 }}
-                            animate={{ rotate: 0 }}
-                            transition={{ duration: 0.3 }}
-                            className="absolute inset-0 z-10 rounded-lg bg-primary"
-                          />
-                          <BriefcaseBusiness size={18} className="relative z-20" />
-                        </div>
-
-                        <span className="flex flex-col">
-                          <p className="text-lg">Moi</p>
-                          <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                            Qui suis-je et ce que je fais ?
-                          </p>
-                        </span>
-                      </li>
-                      <li
-                        className="flex w-full cursor-pointer items-center rounded-xl bg-neutral-100 p-2"
-                        onClick={() => handleNavigation('/about/universe', 'Mon univers')}
-                      >
-                        <NotebookPen className="mr-4" size={18} />
-                        <span className="flex flex-col">
-                          <p className="text-lg">Mon univers</p>
-                          <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                            Découvrez mon univers et mes passions
-                          </p>
-                        </span>
-                      </li>
-                      <li
-                        className="flex w-full cursor-pointer items-center rounded-xl bg-neutral-100 p-2"
-                        onClick={() => handleNavigation('/about/build-in-public', 'Build in public')}
-                      >
-                        <Mouse className="mr-2" size={18} />
-                        <span className="flex flex-col">
-                          <p className="text-lg">Build in public</p>
-                          <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                            Suivez mes posts sur Twitter / LinkedIn
-                          </p>
-                        </span>
-                      </li>
-                    </ul>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </div>
-
-            {/* --- Other tabs ------------------------------------------------------------------------------------ */}
             {[
+              { title: 'À propos', path: '/' },
               { title: 'Services', path: '/services' },
               { title: 'Projets', path: '/projects' },
               { title: 'Contact', path: 'https://cal.com/ugolin-olle' },
